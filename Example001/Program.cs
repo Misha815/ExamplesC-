529132c74b91f2bf69d63f6f759653c5ea6ad710
Mikhael Stepnov <71971590+Misha815@users.noreply.github.com>
2022-10-11 20:14:32 +0300

    Example001Console

---
+++ Example001/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string name;
+name = Console.ReadLine();
+Console.WriteLine("Привет,");
+Console.Write(name);
+Console.ReadKey();
